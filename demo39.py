5155b8367f1a2dcd1706b5a3f5ddfbec151c1bb9
yuan9410 <438191837@qq.com>
2019-03-21 22:45:50 +0800

    adding another new file

---
+++ demo39.py
@@ -0,0 +1,10 @@
+a = ["The Walking Dead", "Entourage", "The Sopranos",
+"The Vampire Diaries"]
+for i in a:
+    print(i)
+
+for i in range(25,51):
+    print(i)
+
+for x,y in enumerate(a):
+    print(x,y)
